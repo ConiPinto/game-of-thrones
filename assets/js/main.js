7b486957b7f803aba45e33a0de1059a78bcba23e
ConiPinto <c.pinjazz@gmail.com>
2019-01-19 16:54:36 -0300

    logo con class, se abren links en otra ventana, se creó root

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,18 @@
  * Acá va solo el manejo del DOM
  * 
  */
+const rootContainer = document.getElementById ("root");
+ const allData = (DATA.characters);
+
+ const showData = (allData) => {
+ let result = "";
+
+ allData.forEach(element => {
+    if (element.characterName !== '') {
+        result = rootContainer
+     
+ });
+ 
+
+}
+ 
