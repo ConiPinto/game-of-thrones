8b4a063c4ab6f7b64ae205326b2573b64048dcf6
ConiPinto <c.pinjazz@gmail.com>
2019-01-20 18:03:00 -0300

    Botón subir funcionando

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -60,3 +60,17 @@ seasonsSelect.addEventListener("change",() => {
     
     
     
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+           document.getElementById("btnTop").style.display = "block";
+       } else {
+           topFunction();
+           document.getElementById("btnTop").style.display = "none";
+       }
+    }
+    function topFunction() {
+       document.body.scrollTop = 0; // For Safari
+       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
